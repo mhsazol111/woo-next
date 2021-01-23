4c1ee0d95a5486b67ee0a2b009b64bf5a100636b
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2021-01-24 00:47:55 +0600

    react-smooth-scrollbar package removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import NextNprogress from 'nextjs-progressbar';
 
-// import Scrollbar from 'react-smooth-scrollbar';
-
 import Head from 'next/head';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
@@ -16,22 +14,14 @@ function MyApp({ Component, pageProps, router }) {
       </Head>
       <div id="page-container">
         <div className="full-page-bg" style={{ backgroundImage: 'url(/images/bg-light-blue.svg)' }} />
-
-        {/* <div className="full-gradient-bg" /> */}
-        {/* <div className="page-inner-container min-h-screen 2xl:container relative 2xl:p-12 md:p-10 p-9"> */}
-        {/* <div className="full-glass-bg"></div> */}
-
-//         <Scrollbar style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}>
+        <NextNprogress color="#95bae8" stopDelayMs={100} height="2" />
         <Header />
         <div id="main-content">
-          <NextNprogress color="#95bae8" stopDelayMs={100} height="2" />
           <AnimatePresence exitBeforeEnter>
             <Component {...pageProps} key={router.route} />
           </AnimatePresence>
         </div>
         <Footer />
-//         </Scrollbar>
-        {/* </div> */}
       </div>
     </>
   );
