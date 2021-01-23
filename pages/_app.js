666021f22c94fc6feb5b84cae478bb613c688df8
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2021-01-23 23:44:51 +0600

    removed scrollbar for performance testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { AnimatePresence } from 'framer-motion';
 import NextNprogress from 'nextjs-progressbar';
 
-import Scrollbar from 'react-smooth-scrollbar';
+// import Scrollbar from 'react-smooth-scrollbar';
 
 import Head from 'next/head';
 import Header from '../src/components/Header';
@@ -21,16 +21,16 @@ function MyApp({ Component, pageProps, router }) {
         {/* <div className="page-inner-container min-h-screen 2xl:container relative 2xl:p-12 md:p-10 p-9"> */}
         {/* <div className="full-glass-bg"></div> */}
 
-        <Scrollbar style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}>
-          <Header />
-          <div id="main-content">
-            <NextNprogress color="#95bae8" stopDelayMs={100} height="2" />
-            <AnimatePresence exitBeforeEnter>
-              <Component {...pageProps} key={router.route} />
-            </AnimatePresence>
-          </div>
-          <Footer />
-        </Scrollbar>
+//         <Scrollbar style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}>
+        <Header />
+        <div id="main-content">
+          <NextNprogress color="#95bae8" stopDelayMs={100} height="2" />
+          <AnimatePresence exitBeforeEnter>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </div>
+        <Footer />
+//         </Scrollbar>
         {/* </div> */}
       </div>
     </>
